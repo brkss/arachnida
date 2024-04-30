c5d299f16fe3782344f67109b9f6c61bb85deac3
Brahim Berkasse <berkassebrahim@gmail.com>
2024-04-30 16:33:16 +0100

    parse program flags (-r, -l and -p) , take url as argument

--- spider/main.go
+++ spider/main.go
@@ -1,17 +1,34 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 
 	"github.com/brkss/cybersecurity-pool/spider/download"
 	"github.com/gocolly/colly"
 )
 
-const URL = "https://en.wikipedia.org/wiki/Luis_Walter_Alvarez"
+//const URL = "https://en.wikipedia.org/wiki/Luis_Walter_Alvarez"
 
 func main() {
 
-	// testing : "https://en.wikipedia.org/wiki/Main_Page"
+	recursive := flag.Bool("r", false, "Recursively download images")
+	maxDepth := flag.Int("l", 5, "Maximum depth for recursive download")
+	savePath := flag.String("p", "./data/", "Path to save downloaded files")
+
+	flag.Parse()
+	args := flag.Args()
+	if len(args) == 0 {
+		log.Fatal("URL is required!")
+	}
+
+	URL := args[0]
+
+	fmt.Println("-r : ", *recursive)
+	fmt.Println("-l : ", *maxDepth)
+	fmt.Println("-p : ", *savePath)
+	fmt.Println("URL : ", URL)
 
 	c := colly.NewCollector()
 
